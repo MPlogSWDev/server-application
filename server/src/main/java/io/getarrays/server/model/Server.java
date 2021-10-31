9669d2880002549e3e25d0c514ddc8e55633f1f3
Matt Plog <MPlogSWDev@outlook.com>
2021-10-31 14:35:18 -0500

    Initial commit

---
+++ server/src/main/java/io/getarrays/server/model/Server.java
@@ -0,0 +1,11 @@
+package io.getarrays.server.model;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    
+}
